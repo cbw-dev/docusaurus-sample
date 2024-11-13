229df914d9ac3467264d0246620c742b70485d75
Julia Qiu <julia.qiu.17@gmail.com>
2024-11-13 14:41:49 -0500

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://cbw-dev.github.io/docusaurus-sample',
+  url: 'https://cbw-dev.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'docusaurus-sample/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
